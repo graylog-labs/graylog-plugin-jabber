3475f46a7d9cd2a57a06b14a0196f65307d8ef8a
Jochen Schalanda <jochen@graylog.com>
2017-02-28 14:24:58 +0100

    Upgrade to Graylog 2.2.0

--- src/main/java/org/graylog2/alarmcallbacks/jabber/JabberAlarmCallbackMetadata.java
+++ src/main/java/org/graylog2/alarmcallbacks/jabber/JabberAlarmCallbackMetadata.java
@@ -9,6 +9,8 @@ import java.util.Collections;
 import java.util.Set;
 
 public class JabberAlarmCallbackMetadata implements PluginMetaData {
+    private static final String PLUGIN_PROPERTIES = "org.graylog.plugins.graylog-plugin-jabber/graylog-plugin.properties";
+
     @Override
     public String getUniqueId() {
         return JabberAlarmCallback.class.getCanonicalName();
@@ -31,7 +33,7 @@ public class JabberAlarmCallbackMetadata implements PluginMetaData {
 
     @Override
     public Version getVersion() {
-        return new Version(1, 2, 1);
+        return Version.fromPluginProperties(getClass(), PLUGIN_PROPERTIES, "version", Version.from(2, 0, 0));
     }
 
     @Override
@@ -41,7 +43,7 @@ public class JabberAlarmCallbackMetadata implements PluginMetaData {
 
     @Override
     public Version getRequiredVersion() {
-        return new Version(2, 0, 0);
+        return Version.fromPluginProperties(getClass(), PLUGIN_PROPERTIES, "graylog.version", Version.from(2, 2, 0));
     }
 
     @Override
--- src/main/java/org/graylog2/alarmcallbacks/jabber/JabberAlarmCallbackPlugin.java
+++ src/main/java/org/graylog2/alarmcallbacks/jabber/JabberAlarmCallbackPlugin.java
@@ -1,5 +1,6 @@
 package org.graylog2.alarmcallbacks.jabber;
 
+import com.google.auto.service.AutoService;
 import org.graylog2.plugin.Plugin;
 import org.graylog2.plugin.PluginMetaData;
 import org.graylog2.plugin.PluginModule;
@@ -7,6 +8,7 @@ import org.graylog2.plugin.PluginModule;
 import java.util.Collection;
 import java.util.Collections;
 
+@AutoService(Plugin.class)
 public class JabberAlarmCallbackPlugin implements Plugin {
     @Override
     public Collection<PluginModule> modules() {
