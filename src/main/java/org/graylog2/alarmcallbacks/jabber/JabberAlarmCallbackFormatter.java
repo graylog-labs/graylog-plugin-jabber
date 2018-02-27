08ac03f9fc3dae37a555b4f108c74aadce219971
Jochen Schalanda <jochen@graylog.com>
2018-02-27 18:13:24 +0100

    Add additional information in Jabber message
    Refs #12

--- src/main/java/org/graylog2/alarmcallbacks/jabber/JabberAlarmCallbackFormatter.java
+++ src/main/java/org/graylog2/alarmcallbacks/jabber/JabberAlarmCallbackFormatter.java
@@ -1,14 +1,39 @@
 package org.graylog2.alarmcallbacks.jabber;
 
+import org.graylog2.plugin.MessageSummary;
 import org.graylog2.plugin.alarms.AlertCondition;
 import org.graylog2.plugin.streams.Stream;
+import org.joda.time.DateTime;
 
 public class JabberAlarmCallbackFormatter {
     public String render(final Stream stream, final AlertCondition.CheckResult result) {
+        final StringBuilder messageBacklog = new StringBuilder();
+        if (result.getMatchingMessages().size() == 0) {
+            messageBacklog.append("No message backlog available.");
+        } else {
+            for (MessageSummary message : result.getMatchingMessages()) {
+                messageBacklog
+                        .append(message.getTimestamp())
+                        .append(" - ")
+                        .append(message.getMessage())
+                        .append("\n");
+            }
+        }
+
+        final AlertCondition condition = result.getTriggeredCondition();
+        final String triggeredCondition = condition == null ? "" :
+                "Trigger: " + condition.getTitle() + " (" + condition.getType() + ")" + "\n"
+                + "Triggered condition: " + condition + "\n";
+
+        final DateTime resultTriggeredAt = result.getTriggeredAt();
+        final String triggeredAt = resultTriggeredAt == null ? "" : "Date: " + resultTriggeredAt + "\n";
+
         return "Graylog alert for stream <" + stream.getTitle() + ">\n\n"
-                + "Date: " + result.getTriggeredAt() + "\n"
                 + "Stream ID: " + stream.getId() + "\n"
-                + "Stream title: " + stream.getTitle() + "\n"
-                + "Triggered condition: " + result.getTriggeredCondition() + "\n";
+                + triggeredAt
+                + triggeredCondition
+                + "\n"
+                + "---- Messages ----" + "\n"
+                + messageBacklog.toString();
     }
 }
--- src/test/java/org/graylog2/alarmcallbacks/jabber/JabberAlarmCallbackFormatterTest.java
+++ src/test/java/org/graylog2/alarmcallbacks/jabber/JabberAlarmCallbackFormatterTest.java
@@ -1,20 +1,108 @@
 package org.graylog2.alarmcallbacks.jabber;
 
 import org.graylog2.alerts.AbstractAlertCondition;
+import org.graylog2.alerts.types.DummyAlertCondition;
+import org.graylog2.plugin.Message;
+import org.graylog2.plugin.MessageSummary;
+import org.graylog2.plugin.alarms.AlertCondition;
 import org.graylog2.plugin.streams.Stream;
+import org.joda.time.DateTime;
 import org.junit.Test;
+
+import java.util.Collections;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class JabberAlarmCallbackFormatterTest {
-
     @Test
-    public void testRender() {
+    public void testRenderWithNegativeCheckResult() {
         final Stream mockStream = mock(Stream.class);
+        when(mockStream.getTitle()).thenReturn("Stream Title");
+        when(mockStream.getId()).thenReturn("001122334455667788");
+
         final AbstractAlertCondition.NegativeCheckResult checkResult = new AbstractAlertCondition.NegativeCheckResult();
         final JabberAlarmCallbackFormatter formatter = new JabberAlarmCallbackFormatter();
 
-        assertThat(formatter.render(mockStream, checkResult)).isNotEmpty();
+        final String s = formatter.render(mockStream, checkResult);
+        assertThat(s)
+                .isNotEmpty()
+                .startsWith("Graylog alert for stream <Stream Title>")
+                .contains("Stream ID: 001122334455667788")
+                .endsWith("No message backlog available.");
+    }
+
+    @Test
+    public void testRenderWithoutBacklog() {
+        final Stream mockStream = mock(Stream.class);
+        when(mockStream.getTitle()).thenReturn("Stream Title");
+        when(mockStream.getId()).thenReturn("001122334455667788");
+        when(mockStream.toString()).thenReturn("Mock-Stream-toString");
+
+        final DummyAlertCondition alertCondition = new DummyAlertCondition(
+                mockStream,
+                "id",
+                DateTime.parse("2018-02-27T17:00:00.000Z"),
+                "admin",
+                Collections.emptyMap(),
+                "title"
+        );
+        final AlertCondition.CheckResult checkResult = new AbstractAlertCondition.CheckResult(
+                true,
+                alertCondition,
+                "result-description",
+                DateTime.parse("2018-02-27T17:00:00.000Z"),
+                Collections.emptyList()
+        );
+        final JabberAlarmCallbackFormatter formatter = new JabberAlarmCallbackFormatter();
+
+        final String s = formatter.render(mockStream, checkResult);
+        assertThat(s)
+                .isNotEmpty()
+                .startsWith("Graylog alert for stream <Stream Title>")
+                .containsSequence(
+                        "Stream ID: 001122334455667788",
+                        "Date: 2018-02-27T17:00:00.000Z",
+                        "Trigger: title (dummy)",
+                        "Triggered condition: id:dummy={Dummy alert to test notifications}, stream:={Mock-Stream-toString}")
+                .endsWith("No message backlog available.");
+    }
+
+    @Test
+    public void testRenderWithBacklog() {
+        final Stream mockStream = mock(Stream.class);
+        when(mockStream.getTitle()).thenReturn("Stream Title");
+        when(mockStream.getId()).thenReturn("001122334455667788");
+        when(mockStream.toString()).thenReturn("Mock-Stream-toString");
+
+        final DummyAlertCondition alertCondition = new DummyAlertCondition(
+                mockStream,
+                "id",
+                DateTime.parse("2018-02-27T17:00:00.000Z"),
+                "admin",
+                Collections.emptyMap(),
+                "title"
+        );
+        final MessageSummary messageSummary = new MessageSummary("graylog_0", new Message("message", "source", DateTime.parse("2018-02-27T17:00:00.000Z")));
+        final AlertCondition.CheckResult checkResult = new AbstractAlertCondition.CheckResult(
+                true,
+                alertCondition,
+                "result-description",
+                DateTime.parse("2018-02-27T17:00:00.000Z"),
+                Collections.singletonList(messageSummary)
+        );
+        final JabberAlarmCallbackFormatter formatter = new JabberAlarmCallbackFormatter();
+
+        final String s = formatter.render(mockStream, checkResult);
+        assertThat(s)
+                .isNotEmpty()
+                .startsWith("Graylog alert for stream <Stream Title>")
+                .containsSequence(
+                        "Stream ID: 001122334455667788",
+                        "Date: 2018-02-27T17:00:00.000Z",
+                        "Trigger: title (dummy)",
+                        "Triggered condition: id:dummy={Dummy alert to test notifications}, stream:={Mock-Stream-toString}")
+                .endsWith("2018-02-27T17:00:00.000Z - message\n");
     }
 }
